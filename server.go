5032e134d82b42f413f950d02d404f8c02bcf21d
David Wickes <dave.wickes@gmail.com>
2022-02-17 23:56:30 +0000

    retrieving/not retrieving a sheet

--- server.go
+++ server.go
@@ -1,12 +1,1 @@
 package backsheet
-
-import (
-	"net/http"
-)
-
-func Server(response http.ResponseWriter, request *http.Request) {
-	if request.URL.Path == "/" {
-		return
-	}
-	response.WriteHeader(http.StatusNotFound)
-}
--- server_test.go
+++ server_test.go
@@ -1,6 +1,8 @@
 package backsheet
 
 import (
+	"errors"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,9 +10,10 @@ import (
 
 func TestServing(t *testing.T) {
 	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	writer := httptest.NewRecorder()
-	Server(writer, request)
-	res := writer.Result()
+	responseRecorder := httptest.NewRecorder()
+	server := newServer(newStubSpreadsheet(""))
+	server.ServeHTTP(responseRecorder, request)
+	res := responseRecorder.Result()
 	if res.StatusCode != http.StatusOK {
 		t.Errorf("Expected status of 200, but got %s", res.Status)
 	}
@@ -19,10 +22,80 @@ func TestServing(t *testing.T) {
 func TestMissingSheet(t *testing.T) {
 	path := "/some-random-path"
 	request := httptest.NewRequest(http.MethodGet, path, nil)
-	writer := httptest.NewRecorder()
-	Server(writer, request)
-	res := writer.Result()
+	responseRecorder := httptest.NewRecorder()
+	server := newServer(StubMissingSpreadsheet{})
+	server.ServeHTTP(responseRecorder, request)
+	res := responseRecorder.Result()
 	if res.StatusCode != http.StatusNotFound {
 		t.Errorf("Expected status of 404, but got %s", res.Status)
 	}
+}
+
+func TestFindingASheet(t *testing.T) {
+	want := "I am some JSON"
+	server := newServer(newStubSpreadsheet(want))
+	path := "/some-known-path"
+	request := httptest.NewRequest(http.MethodGet, path, nil)
+	res := httptest.NewRecorder()
+	server.ServeHTTP(res, request)
+	body, err := ioutil.ReadAll(res.Result().Body)
+	if err != nil {
+		t.Errorf("Unexpected error when reading response body: %s", err)
+	}
+	got := string(body)
+	if got != want {
+		t.Errorf("Expected response body of %s, but got %s", want, got)
+	}
+}
+
+func newServer(spreadsheet Spreadsheet) Server {
+	return Server{spreadsheet}
+}
+
+type Server struct {
+	ss Spreadsheet
+}
+
+func (s Server) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	sheet, err := s.ss.Sheet(req.URL.Path)
+	if err != nil {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+	res.Write([]byte(sheet.ToJSON()))
+}
+
+func newStubSpreadsheet(json string) StubSpreadsheet {
+	return StubSpreadsheet{StubSheet{json}}
+}
+
+type StubSpreadsheet struct {
+	sheet Sheet
+}
+
+type StubMissingSpreadsheet struct {
+}
+
+func (s StubMissingSpreadsheet) Sheet(_ string) (Sheet, error) {
+	return StubSheet{}, errors.New("")
+}
+
+func (ss StubSpreadsheet) Sheet(_ string) (Sheet, error) {
+	return ss.sheet, nil
+}
+
+type StubSheet struct {
+	json string
+}
+
+func (s StubSheet) ToJSON() string {
+	return s.json
+}
+
+type Sheet interface {
+	ToJSON() string
+}
+
+type Spreadsheet interface {
+	Sheet(string) (Sheet, error)
 }
